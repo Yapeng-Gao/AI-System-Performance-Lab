b4d66012074b569ca224dca802e9ce36948e77ab
Yapeng-Gao <593119530@qq.com>
2025-12-23 14:34:24 +0800

    只在 Windows 下给 NVCC 传 -Xcompiler=/wd4819，Linux/macOS 不再传这个参数。

--- cmake/NVCCFlags.cmake
+++ cmake/NVCCFlags.cmake
@@ -5,15 +5,27 @@ if(CMAKE_CUDA_COMPILER_VERSION VERSION_LESS 12.0)
     message(FATAL_ERROR "CUDA version ${CMAKE_CUDA_COMPILER_VERSION} < 12.0. This project requires CUDA 12.0 or higher.")
 endif()
 
-# 基础 Flags
-set(CUDA_BASE_FLAGS
-        "--use_fast_math"
-        "--expt-relaxed-constexpr"
-        "--expt-extended-lambda"
-        "-Xcompiler=/wd4819"  # Windows下忽略特定警告
-        "-Xptxas=-v"          # 输出寄存器使用情况
-        "-lineinfo"           # Nsight 分析支持
-)
+# 基础 Flags（区分 Windows 和 非 Windows）
+if(WIN32)
+    # Windows: 使用 MSVC 风格的 /wd4819，通过 -Xcompiler 传给主机编译器
+    set(CUDA_BASE_FLAGS
+            "--use_fast_math"
+            "--expt-relaxed-constexpr"
+            "--expt-extended-lambda"
+            "-Xcompiler=/wd4819"  # Windows下忽略特定警告
+            "-Xptxas=-v"          # 输出寄存器使用情况
+            "-lineinfo"           # Nsight 分析支持
+    )
+else()
+    # Linux/macOS: 不要传 /wd4819（这是 MSVC 专用），否则 gcc/clang 会把它当成“额外源文件”
+    set(CUDA_BASE_FLAGS
+            "--use_fast_math"
+            "--expt-relaxed-constexpr"
+            "--expt-extended-lambda"
+            "-Xptxas=-v"          # 输出寄存器使用情况
+            "-lineinfo"           # Nsight 分析支持
+    )
+endif()
 
 # 针对架构优化
 # 80=A100, 86=RTX3090, 89=RTX4090, 90=H100
